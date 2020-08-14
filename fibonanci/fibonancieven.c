4de151b579c5cad3e54b7055239b54cb367c35b7
phu wadon <pmmp_1599@hotmail.com>
2020-08-14 16:00:31 +0700

    Add project files.

---
+++ fibonanci/fibonancieven.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main() {
+    long long int fibonanci[100], sum = 0;
+    fibonanci[0] = 1; fibonanci[1] = 2;
+    int i = 2;
+    do
+    {
+        fibonanci[i] = fibonanci[i - 1] + fibonanci[i - 2];
+        if (fibonanci[i] > 4000000) break;
+        i++;
+    } while (fibonanci[i - 1] < 4000000);
+
+    for (int x = 0; fibonanci[x] < 4000000;x++) {
+        if (fibonanci[x] % 2 == 0) sum += fibonanci[x];
+    }
+    printf("%Ld", sum);
+}
