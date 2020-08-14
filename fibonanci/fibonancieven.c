2c8c56824bc550557ac78e7e4eeb25827b83db55
phu wadon <pmmp_1599@hotmail.com>
2020-08-14 16:04:27 +0700

    add scanf

--- fibonanci/fibonancieven.c
+++ fibonanci/fibonancieven.c
@@ -1,16 +1,16 @@
 #include<stdio.h>
 int main() {
-    long long int fibonanci[100], sum = 0;
+    long int fibonanci[100], sum = 0,max;
     fibonanci[0] = 1; fibonanci[1] = 2;
     int i = 2;
+    scanf("ld", &max);
     do
     {
         fibonanci[i] = fibonanci[i - 1] + fibonanci[i - 2];
-        if (fibonanci[i] > 4000000) break;
+        if (fibonanci[i] > max) break;
         i++;
-    } while (fibonanci[i - 1] < 4000000);
-
-    for (int x = 0; fibonanci[x] < 4000000;x++) {
+    } while (fibonanci[i - 1] < max);
+    for (int x = 0; fibonanci[x] < max;x++) {
         if (fibonanci[x] % 2 == 0) sum += fibonanci[x];
     }
     printf("%Ld", sum);
